94471da0fc8bd770548c880ac6f6e611ac5ce81a
Felix <felix_reumann@gmx.de>
2022-07-27 09:34:29 +0200

    ok

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -2,14 +2,62 @@ import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from '@mui/material/Button';
 import {useNavigate} from 'react-router-dom';
+import styled from 'styled-components' ;
+import axios from 'axios';
 
-export default function Signup (){
+const LOCAL_STORAGE_KEY = "token";
+
+function Login ({setUser, setToken}) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+
+    
+
+    async function handleLogin(event) {
+        const credentials = {
+            username: username,
+            password: password
+        }
+        const URL = "http://localhost:3001/api/login"
+        const result = await axios(URL, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify(credentials)
+        });
+        console.log(result);
+
+        if (result.status === 401) {
+            console.log("Fehler")
+            setShowErrorMessage(true)
+        } else {
+            // Parsen der JSON Informationen (Erzeugt ein Promise Objekt)
+            const data = await result.json();
+
+            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
+
+            console.log(data)
+            
+            setToken(data)  
+
+            console.log(username);
+            setUser(username)
+            // fetch mit POST und Body
+        }
+
+        
+    }
+
+
 
   const navigate = useNavigate();
 
   function Submit (){
     navigate ('/');
   }
+  
 
   return (
   <div>
@@ -34,3 +82,4 @@ export default function Signup (){
   
   )
 }
+export default Login;
